99203b0508ea04d0586f8f1414f0cb63ce5e84ea
Egor Boyarkin <egrsfn@gmail.com>
2024-04-07 16:04:13 +0300

    Fix Lottery model

--- Models/Components/Lottery.cs
+++ Models/Components/Lottery.cs
@@ -6,4 +6,5 @@ public class Lottery
     public uint MasterLotteryPriceNumber { get; set; }
     public int Count { get; set; }
     public int DailyCount { get; set; }
+    public string LastCountDate { get; set; } = "";
 }
