8e0c931ba9a92926c4eb28a50fdd4be3118745f7
Egor Boyarkin <egrsfn@gmail.com>
2024-04-07 21:32:41 +0300

    Add EnumerableExtensions

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+using System.Numerics;
+
+namespace Edelstein.Data.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> Range<T>(T start, T count)
+        where T : INumber<T>, IMinMaxValue<T>
+    {
+        T max = start - T.One + count;
+
+        if (count < T.Zero || max > T.MaxValue)
+            throw new ArgumentOutOfRangeException(nameof(count));
+
+        if (count == T.Zero)
+            yield break;
+
+        while (start < max)
+        {
+            yield return start;
+            start++;
+        }
+    }
+}
