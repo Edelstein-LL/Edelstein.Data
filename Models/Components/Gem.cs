b5e3da065aac9f9f597513f2820c0d3b030a51da
Egor Boyarkin <egrsfn@gmail.com>
2025-04-22 15:55:25 +0300

    Make Gem.Total a computable property

--- Models/Components/Gem.cs
+++ Models/Components/Gem.cs
@@ -2,7 +2,7 @@ namespace Edelstein.Data.Models.Components;
 
 public class Gem
 {
-    public long Total { get; set; }
+    public long Total => Charge + Free;
     public long Charge { get; set; }
     public long Free { get; set; }
 }
