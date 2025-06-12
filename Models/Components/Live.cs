f6f4f1e0d9a052f855b79b6d1789d044ef929218
Egor Boyarkin <egrsfn@gmail.com>
2025-06-12 21:26:21 +0300

    Add compatibility stub for live limited rewards

--- Models/Components/Live.cs
+++ Models/Components/Live.cs
@@ -1,11 +1,15 @@
 using Edelstein.Data.Msts;
 
+using MongoDB.Bson.Serialization.Attributes;
+
 using System.Text.Json.Serialization;
 
 namespace Edelstein.Data.Models.Components;
 
 public class Live
 {
+    private Dictionary<uint, int>? _remainingLimitedRewards;
+
     public uint MasterLiveId { get; set; }
     public LiveLevel Level { get; set; }
     public int ClearCount { get; set; }
@@ -16,4 +20,18 @@ public class Live
 
     [JsonIgnore]
     public List<LimitedReward> LimitedRewards { get; set; } = [];
+
+    // TODO: Compatibility stub
+    // TODO: Rework to ReceivedLimitedRewards
+    [JsonIgnore]
+    [BsonIgnore]
+    public Dictionary<uint, int> RemainingLimitedRewards
+    {
+        get
+        {
+            _remainingLimitedRewards ??= LimitedRewards.ToDictionary(x => x.MasterRewardId, x => x.Remaining);
+
+            return _remainingLimitedRewards;
+        }
+    }
 }
